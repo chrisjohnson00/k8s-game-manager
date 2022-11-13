9f33f038a376488b1a70b1656b3e0caaead6bdce
Chris Johnson <chrisjohnson00@hotmail.com>
2022-11-12 22:27:18 -0800

    fix: bp routing

--- flaskr/application.py
+++ flaskr/application.py
@@ -5,11 +5,11 @@ from flask import (
 bp = Blueprint('app', __name__, url_prefix='/')
 
 
-@bp.route('/', methods='GET')
+@bp.route('/')
 def index():
     return render_template('app/list.html')
 
 
-@bp.route('/health', methods='GET')
+@bp.route('/health')
 def health():
     return render_template('app/health.html')
