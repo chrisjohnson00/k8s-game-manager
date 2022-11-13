6a48327270a75e5630562a84e721a3d3db9deb21
Chris Johnson <chrisjohnson00@hotmail.com>
2022-11-12 23:17:21 -0800

    feat: list the running games

--- flaskr/application.py
+++ flaskr/application.py
@@ -1,6 +1,8 @@
 from flask import (
     Blueprint, render_template
 )
+from kubernetes import client
+from kubernetes.client.models import V1StatefulSetList, V1ObjectMeta
 
 bp = Blueprint('app', __name__, url_prefix='/')
 
@@ -8,7 +10,14 @@ bp = Blueprint('app', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     namespaces = ['rust', 'minecraft']
-    return render_template('app/list.html')
+    apps_client = client.AppsV1Api()
+    games = []
+    for namespace in namespaces:
+        stateful_sets = apps_client.list_namespaced_stateful_set(namespace=namespace)  # type: V1StatefulSetList
+        for sts in stateful_sets.items:
+            sts_metadata = sts.metadata  # type: V1ObjectMeta
+            games.append({'name': sts_metadata.name, 'type': 'sts', 'namespace': namespace})
+    return render_template('app/list.html', games=games)
 
 
 @bp.route('/health')
