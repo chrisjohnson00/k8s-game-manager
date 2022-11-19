84f590eea379d07844b8a929c5e93081412349d9
Chris Johnson <chrisjohnson00@hotmail.com>
2022-11-18 23:19:50 -0800

    feat: logging

--- game_manager/application.py
+++ game_manager/application.py
@@ -2,7 +2,7 @@ import services.k8s
 import utilities.relative_time
 import os
 from flask import (
-    Blueprint, render_template, redirect, url_for, flash, request
+    Blueprint, render_template, redirect, url_for, flash, request, current_app as app
 )
 
 bp = Blueprint('app', __name__, url_prefix='/')
@@ -23,7 +23,7 @@ def index():
 def restart(deployment_type, namespace, name):
     services.k8s.restart_game_deployment(namespace, deployment_type, name)
     flash(f"Restarted {name}")
-    application.logger.info(f"Restarted {name}")
+    app.logger.info(f"Restarted {name}")
     return redirect(url_for('app.index'))
 
 
@@ -35,7 +35,7 @@ def health():
 @bp.route('/power/<cycle_type>/<deployment_type>/<namespace>/<name>')
 def power_cycle(cycle_type, deployment_type, namespace, name):
     flash(f"Powering {cycle_type} {name}")
-    application.logger.info(f"Powering {cycle_type} {name}")
+    app.logger.info(f"Powering {cycle_type} {name}")
     if cycle_type == "on":
         services.k8s.scale(namespace, deployment_type, name, 1)
     elif cycle_type == "off":
@@ -76,7 +76,7 @@ def list_plugins(deployment_type, namespace, name, game_name):
     if os.path.exists(full_path):
         files = os.listdir(full_path)
     else:
-        application.logger.info(f"Didn't find {full_path}")
+        app.logger.info(f"Didn't find {full_path}")
     return render_template('app/plugin_list.html', files=files, namespace=namespace, name=name, game_name=game_name,
                            deployment_type=deployment_type)
 
@@ -86,6 +86,6 @@ def delete_plugin(deployment_type, namespace, name, game_name):
     args = request.args
     file_name = args.get("file_name")
     flash(f"Deleting {file_name}... but not really, still testing")
-    application.logger.info(f"Deleting {file_name}")
+    app.logger.info(f"Deleting {file_name}")
     return redirect(
         url_for('app.details', namespace=namespace, name=name, game_name=game_name, deployment_type=deployment_type))
